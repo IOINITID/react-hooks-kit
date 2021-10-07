02392f91a3161ee8a907023af5c416ca679a7bbe
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-10-07 22:27:35 +0300

    Updated test for useFirstMount hook

--- src/tests/lifecycles/useFirstMount.test.ts
+++ src/tests/lifecycles/useFirstMount.test.ts
@@ -1,7 +1,35 @@
+import { renderHook } from '@testing-library/react-hooks';
 import { useFirstMount } from '../../index';
 
 describe('Should work correctly', () => {
   it('Should be defined', () => {
     expect(useFirstMount).toBeDefined();
+  });
+
+  it('Should return isFirstMount is true on mount', () => {
+    const { result } = renderHook(() => useFirstMount());
+    const { isFirstMount } = result.current;
+
+    expect(isFirstMount).toBeTruthy();
+  });
+
+  it('Should return isFirstMount is true on unmount', () => {
+    const { result, unmount } = renderHook(() => useFirstMount());
+
+    unmount();
+
+    const { isFirstMount } = result.current;
+
+    expect(isFirstMount).toBeTruthy();
+  });
+
+  it('Should return isFirstMount is false on rerender', () => {
+    const { result, rerender } = renderHook(() => useFirstMount());
+
+    rerender();
+
+    const { isFirstMount } = result.current;
+
+    expect(isFirstMount).toBeFalsy();
   });
 });
