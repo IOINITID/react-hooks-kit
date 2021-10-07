b6a9a8e2d4b960ae7e3f9a4126a224f39539c174
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-10-07 19:25:24 +0300

    Updated useMount hook tests

--- src/tests/lifecycles/useMount.test.ts
+++ src/tests/lifecycles/useMount.test.ts
@@ -1,5 +1,5 @@
 import { renderHook } from '@testing-library/react-hooks';
-import { useMount } from '../../index';
+import { useMount } from '../../hooks/lifecycles/useMount';
 
 const mockCall = jest.fn();
 
