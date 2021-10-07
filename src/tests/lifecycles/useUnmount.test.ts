9a78e6e97bcb9f0a751225b26fd5008402980163
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-10-07 19:34:17 +0300

    Added tests for useUnmount hook

---
+++ src/tests/lifecycles/useUnmount.test.ts
@@ -0,0 +1,36 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useUnmount } from '../../hooks/lifecycles/useUnmount';
+
+const mockCall = jest.fn();
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+describe('Should work correctly', () => {
+  it('Should be defined', () => {
+    expect(useUnmount).toBeDefined();
+  });
+
+  it('Should not call useUnmount on mount', () => {
+    renderHook(() => useUnmount(mockCall));
+
+    expect(mockCall).toBeCalledTimes(0);
+  });
+
+  it('Should call useUnmount callback once on unmount', () => {
+    const { unmount } = renderHook(() => useUnmount(mockCall));
+
+    unmount();
+
+    expect(mockCall).toBeCalledTimes(1);
+  });
+
+  it('Should not call useUnmount callback on rerender', () => {
+    const { rerender } = renderHook(() => useUnmount(mockCall));
+
+    rerender();
+
+    expect(mockCall).toBeCalledTimes(0);
+  });
+});
